fe5ce635c5529feca335c464ce74d838d1285eb7
Arslan Zaidi <arslanzaidipsu@gmail.com>
2020-10-26 22:16:34 -0400

    revisions 10/26/2020

--- germline_ibd/make_grm.R
+++ germline_ibd/make_grm.R
@@ -2,44 +2,67 @@
 #write grm from ibd match file
 library(data.table)
 args=commandArgs(TRUE)
-infile=args[1]
-outfile=args[2]
+infile = args[1]
+popfile = args[2]
+nsnps = args[3]
+outfile = args[4]
 
-dat=fread(infile)
-colnames(dat)=c("chr","iid1","iid2","total_length")
-dat[,total_length:=total_length/200]
+dat = fread(infile)
+colnames(dat) = c("chr","iid1","iid2","total_length")
+dat[, total_length := total_length/200]
 
+pop = fread(popfile, header = FALSE) 
+colnames(pop) = c("FID","IID")
+ninds = nrow(pop)
+pop$iid = seq(1,ninds)
 
-dat[,pair:=paste(iid1,iid2,sep="_")]
-dat2=dat[,sum(total_length),by=c("pair","iid1","iid2")]
+dat[, pair := paste(iid1,iid2,sep="_")]
+dat2 = dat[,sum(total_length),by=c("pair","iid1","iid2")]
 
-all_combs=t(combn(0:8999,2))
-all_combs=data.table(iid1=all_combs[,1],iid2=all_combs[,2])
-all_combs[,pair:=paste(iid1,iid2,sep="_")]
+all_combs=t(combn(pop$iid,2))
+all_combs=data.table(iid1 = all_combs[,1],
+                     iid2 = all_combs[,2])
+all_combs[, pair := paste(iid1,iid2,sep="_")]
 
-all_combs=merge(all_combs,dat2[,c("pair","V1")],by="pair",sort=F,all.x=T)
-all_combs[is.na(all_combs$V1),V1:=0]
-all_combs[,iid1:=iid1+1]
-all_combs[,iid2:=iid2+1]
-all_combs[,nsnp:=15e4]
+all_combs = merge(all_combs,
+                  dat2[, c("pair","V1")],
+                  by = "pair",
+                  sort = FALSE,
+                  all.x = TRUE)
 
-samz=data.table(pair=paste((1:9000),c(1:9000),sep="_"),iid1=1:9000,iid2=1:9000,V1=1,nsnp=150e3)
+all_combs[is.na(all_combs$V1),
+          V1 := 0]
+all_combs[,iid1 := iid1+1]
+all_combs[,iid2 := iid2+1]
+all_combs[,nsnp := nsnps]
+
+samz=data.table(pair = paste(pop$iid+1,
+                             pop$iid+1,
+                             sep="_"),
+                iid1 = pop$iid+1,
+                iid2 = pop$iid+1,
+                V1 = 1,
+                nsnp = nsnps)
 
 all_combs=rbind(all_combs,samz)
 
-all_combs=all_combs[order(iid2,iid1)]
+all_combs=all_combs[order(iid1,iid2)]
 
 #write grm
-fwrite(all_combs[,.(iid2,iid1,nsnp,V1)],
-       paste(outfile,".grm"),
-       col.names=F,row.names=F,quote=F,sep="\t")
+fwrite(all_combs[,.(iid1,iid2,nsnp,V1)],
+       paste(outfile,".grm", sep=""),
+       col.names=FALSE,
+       row.names=FALSE,
+       quote=FALSE,sep="\t")
 
 #write grm id file
-fwrite(data.table(fid=paste("msp_",(1:9000),sep=""),
-                  iid=paste("msp_",(1:9000),sep="")),
-       paste(outfile,".grm.id"),
+fwrite(data.table(fid=pop$FID,
+                  iid=pop$IID),
+       paste(outfile,".grm.id", sep=""),
        sep="\t",
-       col.names=F,row.names=F,quote=F)
+       col.names=FALSE,
+       row.names=FALSE,
+       quote=FALSE)
 
 
 
--- germline_ibd/proc_germline.R
+++ germline_ibd/proc_germline.R
@@ -2,24 +2,85 @@ library(data.table)
 
 args=commandArgs(TRUE)
 
-infile=args[1]
-outfile=args[2]
+infile = args[1]
+popfile = args[2]
+nsnps = args[3]
+outfile=args[4]
 
 dat<-fread(infile)
 
 colnames(dat)=c("f1","f2","chr","position","id2","total_snps","glength","unit","mismatch","ind1hom","ind2hom")
 
+pop = fread(popfile, header=FALSE)
+colnames(pop) = c("FID","IID")
+ninds = nrow(pop)
+pop$sno = seq(1,ninds)
+
 chromosome=dat$chr[1]
 dat[,c("fid1", "iid1") := tstrsplit(f1, " ", fixed=TRUE)][,f1:=NULL]
 dat[,c("fid2", "iid2") := tstrsplit(f2, " ", fixed=TRUE)][,f2:=NULL]
-dat[,c("tsk", "iid1") := tstrsplit(iid1, "_", fixed=TRUE)]
-dat[,c("tsk", "iid2") := tstrsplit(iid2, "_", fixed=TRUE)]
+#dat[,c("tsk", "iid1") := tstrsplit(iid1, "_", fixed=TRUE)]
+#dat[,c("tsk", "iid2") := tstrsplit(iid2, "_", fixed=TRUE)]
 dat[,c("start_bp", "stop_bp") := tstrsplit(position, " ", fixed=TRUE)][,position:=NULL]
-dat[,c("start_rsid", "stop_rsid") := tstrsplit(id2, " ", fixed=TRUE)][,id2:=NULL]
-dat[,"pair":=paste(iid1,iid2,sep="_")]
+#dat[,c("start_rsid", "stop_rsid") := tstrsplit(id2, " ", fixed=TRUE)][,id2:=NULL]
+dat = merge(dat, pop, by.x =c("fid1","iid1"),
+            by.y = c("FID","IID"))
+colnames(dat)[15]="sno1"
 
-dat[,total_length:=sum(glength),by=c("iid1","iid2","pair")]
-dat2=dat[,c('chr','iid1','iid2','total_length'),with=F]
+dat = merge(dat, pop, by.x =c("fid2","iid2"),
+            by.y = c("FID","IID"))
+colnames(dat)[16]="sno2"
 
-fwrite(dat2,
-       outfile,sep="\t",col.names=F,row.names=F,quote=F)
+dat[,"pair":=paste(sno1,sno2,sep="_")]
+
+dat2 = dat[,.(total_length=sum(glength)/200),
+           by=c("pair",'sno1','sno2')]
+dat2=dat2[,c('sno1','sno2','pair','total_length'),with=FALSE]
+
+
+all_combs=t(combn(pop$sno,2))
+all_combs=data.table(sno1 = all_combs[,1],
+                     sno2 = all_combs[,2])
+
+all_combs[, pair := paste(sno1,sno2,sep="_")]
+all_combs = merge(all_combs,
+                  dat2[, c("pair","total_length")],
+                  by = "pair",
+                  sort = FALSE,
+                  all.x = TRUE)
+
+all_combs[is.na(all_combs$total_length),
+          total_length := 0]
+
+all_combs[,nsnp := nsnps]
+
+samz=data.table(pair = paste(pop$sno,
+                             pop$sno,
+                             sep="_"),
+                sno1 = pop$sno,
+                sno2 = pop$sno,
+                total_length = 1,
+                nsnp = nsnps)
+
+all_combs=rbind(all_combs,samz)
+
+all_combs=all_combs[order(sno2,sno1)]
+
+#write grm
+fwrite(all_combs[,.(sno2,sno1,nsnp,total_length)],
+       paste(outfile,".grm", sep=""),
+       col.names=FALSE,
+       row.names=FALSE,
+       quote=FALSE,sep="\t")
+
+#write grm id file
+fwrite(data.table(fid=pop$FID,
+                  iid=pop$IID),
+       paste(outfile,".grm.id", sep=""),
+       sep="\t",
+       col.names=FALSE,
+       row.names=FALSE,
+       quote=FALSE)
+
+
+
